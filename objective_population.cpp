23ebc38c84c01e09c731831782076a3830a381f3
Ben Lubar <ben.lubar@gmail.com>
2020-01-09 14:30:04 -0600

    Implement population objective.

--- objective_population.cpp
+++ objective_population.cpp
@@ -1,5 +1,12 @@
 #include "square.h"
 
+#include "modules/Units.h"
+#include "modules/World.h"
+
+#include "df/world.h"
+
+REQUIRE_GLOBAL(world);
+
 template<>
 bool check_objective<BingoObjective::ADOPTED_ANIMALS>(color_ostream &, BingoSquare &)
 {
@@ -43,22 +50,51 @@ std::string describe_objective<BingoObjective::EVERYONE_IN_CAGES>(const BingoSqu
 }
 
 template<>
-bool check_objective<BingoObjective::POPULATION>(color_ostream &, BingoSquare &)
+bool check_objective<BingoObjective::POPULATION>(color_ostream &, BingoSquare & square)
 {
-    // TODO
+    if (square.state == BingoState::SUCCEEDED || !Core::getInstance().isMapLoaded())
+    {
+        return false;
+    }
+
+    if (!World::isFortressMode())
+    {
+        return false;
+    }
+
+    int population = 0;
+    for (auto & unit : world->units.active)
+    {
+        if (!Units::isSane(unit))
+            continue;
+        if (!Units::isCitizen(unit) && (!unit->flags2.bits.resident || !Units::isOwnGroup(unit)))
+            continue;
+
+        population++;
+    }
+
+    if (population >= square.data1)
+    {
+        square.state = BingoState::SUCCEEDED;
+        return true;
+    }
+
     return false;
 }
 
 template<>
-std::string summarize_objective<BingoObjective::POPULATION>(const BingoSquare &)
+std::string summarize_objective<BingoObjective::POPULATION>(const BingoSquare & square)
 {
-    // TODO
-    return "TODO";
+    std::ostringstream str;
+    str << "Population: " << square.data1;
+    return str.str();
 }
 
 template<>
-std::string describe_objective<BingoObjective::POPULATION>(const BingoSquare &)
+std::string describe_objective<BingoObjective::POPULATION>(const BingoSquare & square)
 {
-    // TODO
-    return "TODO";
+    std::ostringstream str;
+    str << "Reach a population of " << square.data1 << " citizens and permanent residents in your fortress. ";
+    str << "Visitors, animals, and the deceased don't count.";
+    return str.str();
 }
